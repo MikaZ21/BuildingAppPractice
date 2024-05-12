50b010e83d9d733e32e52f9618421af0c4a8eabe
MikaZ21 <zukeyamamika@gmail.com>
2024-05-12 12:59:31 -0500

    Finished Challenge

--- FlowerShopSalesReport/script.js
+++ FlowerShopSalesReport/script.js
@@ -1,11 +1,26 @@
 let sales = [455, 505, 340, 780, 690, 880, 900, 685, 835, 754];
 let customers = [25, 30, 20, 35, 36, 32, 44, 45, 34, 15];
-const daysOfTheWeek = ["Monday", "Thuesday", "wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-let firstWeekSales = sales.filter((sale, i) => i < 7);
-let firstWeekCustomers = customers.filter((customer, i) => i < 7);
+const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
+ // Declare valuables in the global scope so that it's accessible throughout script.
+let firstWeekSales;
+let firstWeekCustomers;
+let tableSales;
+let tableSum;
+let firstWeekSalesSum;
+let firstWeekCustomersSum;
 
-document.addEventListener("DOMContentLoaded", function() { // ensure the script runs after the document content is fully loaded
+document.addEventListener("DOMContentLoaded", function() { // ensure the script runs after the document content is fully loaded.
+    // If script is placed at the end of the '<body>' tag, it might not need to wait for 'DOMContentLoaded' because by the time the script runs, the DOM is already loaded.
+
+    tableSales = document.getElementById("salesTable"); // Access to the table
+    tableSum = document.getElementById("sumOfTable");     
+    firstWeekSales = sales.filter((sale, i) => i < 7); // Creating a new array by filtering the original 'sales' array.
+    firstWeekCustomers = customers.filter((customer, i) => i < 7);
+    firstWeekSalesSum = firstWeekSales.reduce((prev, curr) => prev + curr, 0);
+    firstWeekCustomersSum = firstWeekCustomers.reduce((prev, curr) => prev + curr, 0);
+
+    
     populateTable();
     populateWkSales();
     populateWkCustomers();
@@ -17,10 +32,11 @@ document.addEventListener("DOMContentLoaded", function() { // ensure the script 
 })
 
 function populateTable() {
-    const table = document.getElementById("myTable"); // Access to the table
-    let cells = table.rows[0].querySelectorAll('td'); // Selecting the first row
+    // console.log("Populating table...");
+    let cells = tableSales.rows[0].querySelectorAll('td'); // Selecting all 'td' in the first row of the table.
 
-    daysOfTheWeek.forEach(function(day, index) {
+    daysOfTheWeek.forEach(function(day, index) { // 'day' represents the current day being iterated over, 
+                                                // and 'index' represents the index of that day in the array.
         // cells[index + 1].innerText = day;
         cells[index].innerText = day;
     }); // Loop through each cell and assign the corresponding day
@@ -29,26 +45,18 @@ function populateTable() {
 function populateWkSales() {
     // console.log("Function called"); 
 
-    const table = document.getElementById("myTable"); // Access to the table
-    // console.log("Table found:", table); 
-
-    let cells = table.rows[1].querySelectorAll('td'); // Selecting the second row for first week sales
+    let cells = tableSales.rows[1].querySelectorAll('td'); // Selecting the second row for first week sales.
     // console.log("Cells found:", cells);
-
-    const firstWeekSales = sales.filter((sale, i) => i < 7);
 
     firstWeekSales.forEach(function(sale, index) {
         // cells[index + 1].innerText = day;
-        cells[index].innerText = '$ ' + sale; // Prepend '$' to each sale value
+        cells[index].innerText = new Intl.NumberFormat( 
+            'en-US', {style: 'currency', currency: 'USD' }).format(sale);  // Format the number as currency with comma separator for thousands.
     }); // Loop through each cell and assign the corresponding sales data
 }
 
-
 function populateWkCustomers() {
-
-    const table = document.getElementById("myTable"); 
-    let cells = table.rows[2].querySelectorAll('td'); // Selecting the third row for first week customer's count
-    const firstWeekCustomers = customers.filter((customer, i) => i < 7);
+    let cells = tableSales.rows[2].querySelectorAll('td'); // Selecting the third row for first week customer's count
 
     firstWeekCustomers.forEach(function(customer, index) {
         cells[index].innerText = customer; 
@@ -56,11 +64,8 @@ function populateWkCustomers() {
 }
 
 function salesSum() {
-    const table = document.getElementById("sumOfTable"); 
-    let cells = table.rows[0].querySelectorAll('td'); 
-    let firstWeekSalesSum = firstWeekSales.reduce((prev, curr) => prev + curr, 0);
+    let cells = tableSum.rows[0].querySelectorAll('td'); 
 
-    // Format the number as currency with comma separator for thousands
     let formattedSalesSum = new Intl.NumberFormat( 
         'en-US', {style: 'currency', currency: 'USD' }).format(firstWeekSalesSum);
     
@@ -68,9 +73,7 @@ function salesSum() {
 }
 
 function avgSalesDay() {
-    const table = document.getElementById("sumOfTable"); 
-    let cells = table.rows[0].querySelectorAll('td'); 
-    let firstWeekSalesSum = firstWeekSales.reduce((prev, curr) => prev + curr, 0);
+    let cells = tableSum.rows[0].querySelectorAll('td'); 
     let averageSalesPerDay = firstWeekSalesSum / 7;
 
     // Format the number as currency with comma separator for thousands
@@ -81,20 +84,14 @@ function avgSalesDay() {
 }
 
 function customersSum() {
-    const table = document.getElementById("sumOfTable"); 
-    let cells = table.rows[1].querySelectorAll('td'); 
-    let firstWeekCustomersSum = firstWeekCustomers.reduce((prev, curr) => prev + curr, 0);
+    let cells = tableSum.rows[1].querySelectorAll('td'); 
     
     cells[0].innerText = firstWeekCustomersSum;
 }
 
 function avgCustomerSpendDay() {
-    const table = document.getElementById("sumOfTable"); 
-    let cells = table.rows[1].querySelectorAll('td'); 
-    let firstWeekSalesSum = firstWeekSales.reduce((prev, curr) => prev + curr, 0); 
-    let firstWeekCustomersSum = firstWeekCustomers.reduce((prev, curr) => prev + curr, 0);
+    let cells = tableSum.rows[1].querySelectorAll('td'); 
     let firstWeekCustomerAvgSpend = firstWeekSalesSum / firstWeekCustomersSum;
-
     let formattedAvgCustomers = new Intl.NumberFormat( 
         'en-US', {style: 'currency', currency: 'USD' }).format(firstWeekCustomerAvgSpend);
     
@@ -102,8 +99,8 @@ function avgCustomerSpendDay() {
 }
 
 function avgCustomerSpendByDay() {
-    const table = document.getElementById("myTable"); // Access to the table
-    const avgCustomerRow = table.querySelector(".avgCustomerSpend"); // Selecting the row for average customer spend
+    // Selecting the row for average customer spend.
+    const avgCustomerRow = tableSales.querySelector(".avgCustomerSpendByDay");
 
     for (let i = 0; i < daysOfTheWeek.length; i++) {
         // Calculate the average spend per customer for the current day
